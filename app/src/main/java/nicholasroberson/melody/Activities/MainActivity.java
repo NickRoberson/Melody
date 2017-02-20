e3ae8dfc7efbcd2499b3c59276cabe93e116e1dc
nicholasroberson <roberson@grinnell.edu>
2017-02-20 14:59:43 -0600

    changed manfest

--- app/src/main/java/nicholasroberson/melody/Activities/MainActivity.java
+++ app/src/main/java/nicholasroberson/melody/Activities/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.e("ACTIVITY_STARTED", "Started MainActivity");
+
         tts = new TextToSpeech(this, this);
 
         etData = (EditText) findViewById(R.id.etData);
--- app/src/main/java/nicholasroberson/melody/Splash/SplashActivity.java
+++ app/src/main/java/nicholasroberson/melody/Splash/SplashActivity.java
@@ -1,8 +1,40 @@
 package nicholasroberson.melody.Splash;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import nicholasroberson.melody.Activities.MainActivity;
+
 /**
  * Created by nicholasroberson on 2/20/17.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Log.e("ACTIVITY_STARTED", "Started SplashActivity");
+        Intent result = new Intent(this,MainActivity.class);
+        startActivity(result);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
